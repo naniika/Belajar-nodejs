17c91237e7ab7d4635b05ad35750657188503e5c
Naniika <naniikagh@gmail.com>
2020-01-22 05:21:03 +0700

    ternary > ifelse

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
+const qString = require("querystring");
 
-http
-  .createServer((req, res) => {
-    const access = url.parse(req.url);
-    const route =
-      access.pathname == "/"
-        ? {
-            file: "./tamplates/index.html"
-          }
-        : access.pathname == "/contact"
-        ? {
-            file: "./tamplates/contact.html"
-          }
-        : access.pathname == "/about"
-        ? {
-            file: "./tamplates/about.html"
-          }
-        : {
-            file: "./tamplates/404.html"
-          };
+http.createServer((req, res) => {
+  const access = url.parse(req.url);
+  if (access.pathname == "/") {
     res.writeHead(200, {
       "Content-Type": "text/html"
     });
-    fs.createReadStream(route.file).pipe(res);
-  })
-  .listen(8080);
+    fs.createReadStream("./tamplates/index.html").pipe(res);
+  } else if (access.pathname == "/contact") {
+    res.writeHead(200, {
+      "Content-Type": "text/html"
+    });
+    fs.createReadStream("./tamplates/contact.html").pipe(res);
+  } else if (access.pathname == "/about") {
+    res.writeHead(200, {
+      "Content-Type": "text/html"
+    });
+    fs.createReadStream("./tamplates/about.html").pipe(res);
+  } else {
+    res.writeHead(404, {
+      "Content-Type": "text/html"
+    });
+    fs.createReadStream("./tamplates/404.html").pipe(res);
+  }
+}).listen(8080);
 console.log("Server sedang berjalan....");
